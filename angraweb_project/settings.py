15707b1c9b24ee576a241fda08fab82b3ea85481
aliasttt <aliasadi3853@gmail.com>
2026-03-05 14:17:54 +0330

    first commit

--- angraweb_project/settings.py
+++ angraweb_project/settings.py
@@ -206,7 +206,7 @@ LOGOUT_REDIRECT_URL = 'index'
 # Email (ProMail SMTP)
 # Keep credentials in environment variables for security.
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'mail.promail.com.tr')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'mega.promail.com.tr')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'info@angraweb.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
